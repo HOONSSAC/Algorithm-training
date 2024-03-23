da5c3c581625d1a422d3511bf495ef748c2feacd
b1uesoda <b1uesoda@naver.com>
2024-03-23 12:27:29 +0900

    Quick Sort

--- QuickSort/main.c
+++ QuickSort/main.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include "quickSortRandom.h"
 #include "quickSortMedian.h"
+#include "mergeSort.h"
 
 // 배열을 순서대로 출력하는 함수
 void printArr(int arr[]) {
@@ -14,20 +15,34 @@ void printArr(int arr[]) {
     printf("\n");
 }
 
+//1~100난수 배열 생성함수
+void randomNum(int list[], int num) {
+    srand((unsigned)time(NULL));
+    for (int i = 0; i < num; i++) {
+        list[i] = (rand()%100)+1;
+    }
+}
+
 int main() {
     clock_t start, end; // 시간 측정을 위한 변수
-    int arr[MAX] = {6, 3, 11, 9, 12, 2, 8, 15, 18, 10, 7, 14}; // 정렬되지 않은 배열
+    int arr[12] = {6, 3, 11, 9, 12, 2, 8, 15, 18, 10, 7, 14}; // 임의의 배열
+    // int arr[MAX] = { 0 }; // 60개의 입력 배열
+    // randomNum(arr, MAX); // 60개의 랜덤 값 생성
+
+
+
     printArr(arr); // 정렬되지 않은 배열 출력
 
     start = clock(); // 시간 측정 시작
 
-    // 더 정밀한 시간 측정을 위해 정렬을 백만 번 반복한다.
-    for(int i = 0; i < 10000000; i++) {
+    // 더 정밀한 시간 측정을 위해 정렬을 만 번 반복한다.
+    for(int i = 0; i < 100000; i++) {
         int testArr[MAX]; // 정렬된 배열을 저장할 배열
         memcpy(testArr, arr, sizeof(arr)); // 원본 배열을 복사
 
-        QuickSortRandomPivot(testArr, 0, MAX - 1); // random 방식으로 pivot을 선정하는 QuickSort
-        //QuickSortMedianPivot(testArr, 0, MAX - 1); // median 방식으로 pivot을 선정하는 QuickSort
+        //QuickSortRandomPivot(testArr, 0, MAX - 1); // random 방식으로 pivot을 선정하는 QuickSort
+        QuickSortMedianPivot(testArr, 0, MAX - 1); // median 방식으로 pivot을 선정하는 QuickSort
+        //mergeSort(testArr, 0, MAX - 1); // MergeSort
 
         memcpy(arr, testArr, sizeof(arr)); // 다시 원본 배열로 복사
     }
--- QuickSort/setting.h
+++ QuickSort/setting.h
@@ -2,5 +2,5 @@
 #ifndef SETTING_H
 #define SETTING_H
 #define SWAP(a,b){int t; t=a;a=b;b=t;} //값 교환 매크로
-#define MAX 12 // 배열의 길이
+#define MAX 12 // 배열의 길이 (60개의 배열을 정렬할 때는 60으로 변경)
 #endif // SETTING_H
